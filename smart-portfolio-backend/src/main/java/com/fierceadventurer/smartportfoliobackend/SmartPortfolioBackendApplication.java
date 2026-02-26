19f52dd817833e1610477e7c890179ef3948807b
fierce-adventurer <tripathiayush589@gmail.com>
2026-02-27 02:20:12 +0530

    feat: initialized the spring project 3.5.11, updated all the required dependency for the project.

---
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/SmartPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fierceadventurer.smartportfoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPortfolioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPortfolioBackendApplication.class, args);
+    }
+
+}
---
+++ smart-portfolio-backend/src/test/java/com/fierceadventurer/smartportfoliobackend/SmartPortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fierceadventurer.smartportfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPortfolioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
