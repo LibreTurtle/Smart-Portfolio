f32edd2df404b65ea6cc76953a1e235085af5d60
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-07 00:52:37 +0530

    fix: updated the application.yml according to pom.xml and fixed minor typo in ContactRepository.java

--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/Repository/ContactRepository.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/Repository/ContactRepository.java
@@ -2,10 +2,12 @@ package com.fierceadventurer.smartportfoliobackend.content.Repository;
 
 import com.fierceadventurer.smartportfoliobackend.content.entity.ContactMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface ContactRepository extends JpaRepository<ContactMessage , UUID> {
-    List<ContactMessage> findByIsRealFalseOrderBySubmittedAtDesc();
+    List<ContactMessage> findByIsReadFalseOrderBySubmittedAtDesc();
 }
