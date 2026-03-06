c71b0f60d60e1031ef1f6332082e7889dcd6aa8c
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-07 01:51:12 +0530

    feat: created schema.sql for creating tables in neondb

---
+++ smart-portfolio-backend/src/main/resources/schema.sql
@@ -0,0 +1,59 @@
+CREATE EXTENSION IF NOT EXISTS  vector;
+
+CREATE TABLE IF NOT EXISTS projects (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    tech_stack VARCHAR(255),
+    github_url VARCHAR(255),
+    live_url VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS contact_messages(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    sender_name VARCHAR(100) NOT NULL,
+    sender_email VARCHAR(255) NOT NULL,
+    message_body TEXT NOT NULL,
+    is_read BOOLEAN DEFAULT FALSE,
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS resume_embeddings(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    content TEXT NOT NULL,
+    embedding VECTOR(768) NOT NULL,
+    metadata JSONB
+);
+
+CREATE INDEX IF NOT EXISTS resume_embeddings_hnsw_idx ON resume_embeddings USING hnsw (embedding vector_cosine_ops);
+
+CREATE TABLE IF NOT EXISTS ai_semantic_cache(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    prompt_text TEXT NOT NULL,
+    prompt_embedding VECTOR(768) NOT NULL,
+    cached_response TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS sponsors(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    sponsor_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    amount DECIMAL(10 , 2) NOT NULL,
+    currency VARCHAR(3) DEFAULT 'INR',
+    status VARCHAR(50) DEFAULT 'PENDING',
+    stripe_session_id VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS outbox_events (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    aggregate_type VARCHAR(100) NOT NULL,
+    aggregate_id VARCHAR(255) NOT NULL,
+    event_type VARCHAR(100) NOT NULL,
+    payload JSONB NOT NULL,
+    is_processed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    event_id VARCHAR(255) UNIQUE
+)
