a16d450b7406dc7977b23af61901d81b7c14bfab
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-07 21:49:45 +0530

    feat: created mapper class using mapstruct and added its dependency in pom.xml

---
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/controller/ProjectController.java
@@ -0,0 +1,13 @@
+package com.fierceadventurer.smartportfoliobackend.content.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+
+    @
+}
