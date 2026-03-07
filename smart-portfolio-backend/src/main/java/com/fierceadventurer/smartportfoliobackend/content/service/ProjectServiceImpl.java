eab6765bf893197999d4a38c0562b3aa379cd585
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-07 22:48:11 +0530

    feat: implemented service logic to upload a new project to the portfolio.

--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/service/ProjectServiceImpl.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/service/ProjectServiceImpl.java
@@ -1,10 +1,12 @@
 package com.fierceadventurer.smartportfoliobackend.content.service;
 
+import com.fierceadventurer.smartportfoliobackend.content.dto.ProjectRequest;
 import com.fierceadventurer.smartportfoliobackend.content.repository.ProjectRepository;
 import com.fierceadventurer.smartportfoliobackend.content.dto.ProjectResponse;
 import com.fierceadventurer.smartportfoliobackend.content.mapper.ProjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +29,16 @@ public class ProjectServiceImpl implements ProjectService {
         return projectRepository.findAll().stream()
                 .map(projectMapper::toResponse)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    @CacheEvict(value = "portfolio_projects", allEntries = true)
+    public ProjectResponse createProject(ProjectRequest request) {
+        log.info("Creating a new project: {}", request.title());
+
+        var entity = projectMapper.toEntity(request);
+        var savedEntity = projectRepository.save(entity);
+
+        return projectMapper.toResponse(savedEntity);
     }
 }
