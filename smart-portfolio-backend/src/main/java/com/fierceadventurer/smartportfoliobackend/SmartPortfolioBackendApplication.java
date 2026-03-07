fcc1d4bdcd6f5f54b389b2c58f8346bd76dc5001
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-07 21:55:35 +0530

    feat: enable in memory caching using caffiene in spring.

--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/SmartPortfolioBackendApplication.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/SmartPortfolioBackendApplication.java
@@ -2,8 +2,10 @@ package com.fierceadventurer.smartportfoliobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SmartPortfolioBackendApplication {
 
     public static void main(String[] args) {
