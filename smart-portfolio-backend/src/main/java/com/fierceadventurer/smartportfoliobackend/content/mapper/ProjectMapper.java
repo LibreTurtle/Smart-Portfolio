4556c3798b8c0fba5445d15e602a3595188b9162
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-07 22:42:40 +0530

    feat: added a new mapping for toentity from requestDto.

--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/mapper/ProjectMapper.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/mapper/ProjectMapper.java
@@ -1,11 +1,17 @@
 package com.fierceadventurer.smartportfoliobackend.content.mapper;
 
+import com.fierceadventurer.smartportfoliobackend.content.dto.ProjectRequest;
 import com.fierceadventurer.smartportfoliobackend.content.dto.ProjectResponse;
 import com.fierceadventurer.smartportfoliobackend.content.entity.Project;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProjectMapper {
     ProjectResponse toResponse(Project project);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    Project toEntity(ProjectRequest request);
 }
