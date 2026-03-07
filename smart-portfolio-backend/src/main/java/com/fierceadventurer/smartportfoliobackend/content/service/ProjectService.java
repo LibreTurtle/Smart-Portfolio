52f6e3d1d99e43525bdf6938a26ad0be1c411381
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-07 22:45:19 +0530

    feat: added a new method to create a project in website.

--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/service/ProjectService.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/service/ProjectService.java
@@ -1,9 +1,12 @@
 package com.fierceadventurer.smartportfoliobackend.content.service;
 
+import com.fierceadventurer.smartportfoliobackend.content.dto.ProjectRequest;
 import com.fierceadventurer.smartportfoliobackend.content.dto.ProjectResponse;
 
 import java.util.List;
 
 public interface ProjectService {
     List<ProjectResponse> getAllProjects();
+
+    ProjectResponse createProject(ProjectRequest request);
 }
