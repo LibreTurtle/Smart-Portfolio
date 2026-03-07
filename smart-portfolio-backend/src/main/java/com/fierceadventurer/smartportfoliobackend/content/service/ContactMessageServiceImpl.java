445c6f2d86576171758d8a39ba010e28376604ad
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-07 22:22:47 +0530

    feat: wrote service logic in ContactMessageServiceImpl for saving query and returning response.

---
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/content/service/ContactMessageServiceImpl.java
@@ -0,0 +1,29 @@
+package com.fierceadventurer.smartportfoliobackend.content.service;
+
+import com.fierceadventurer.smartportfoliobackend.content.dto.ContactMessageRequest;
+import com.fierceadventurer.smartportfoliobackend.content.dto.ContactMessageResponse;
+import com.fierceadventurer.smartportfoliobackend.content.mapper.ContactMessageMapper;
+import com.fierceadventurer.smartportfoliobackend.content.repository.ContactRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ContactMessageServiceImpl  implements ContactMessageService{
+
+    private final ContactRepository repository;
+    private final ContactMessageMapper mapper;
+
+    @Override
+    public ContactMessageResponse submitMessage(ContactMessageRequest request) {
+        log.info("Recived new contact message form : {}", request.senderEmail());
+
+        var entity = mapper.toEntity(request);
+
+        var savedEntity = repository.save(entity);
+
+        return mapper.toResponse(savedEntity);
+    }
+}
