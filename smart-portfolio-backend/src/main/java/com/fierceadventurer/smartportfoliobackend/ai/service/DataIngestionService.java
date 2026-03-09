1fca1a4c62642eb3d1316024efa14fde5301ae0e
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-10 00:14:00 +0530

    feat: wrote a method interface to read pdf.

--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/service/DataIngestionService.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/service/DataIngestionService.java
@@ -3,5 +3,5 @@ package com.fierceadventurer.smartportfoliobackend.ai.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface DataIngestionService {
-    void ingestpdf(MultipartFile file);
+    void ingestPdf(MultipartFile file);
 }
