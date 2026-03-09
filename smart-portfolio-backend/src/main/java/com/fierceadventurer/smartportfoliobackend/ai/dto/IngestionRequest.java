06e60744dccc36b23572880dfb4ea61993fae8bd
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-10 00:12:50 +0530

    feat: added spring document reader for reading resume pdf.

--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/dto/IngestionRequest.java
+++
@@ -1,9 +0,0 @@
-package com.fierceadventurer.smartportfoliobackend.ai.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record IngestionRequest(
-        @NotBlank(message = "content cannot be empty")
-        String content
-) {
-}
--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/service/DataIngestionService.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/service/DataIngestionService.java
@@ -1,5 +1,7 @@
 package com.fierceadventurer.smartportfoliobackend.ai.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface DataIngestionService {
-    void ingestData(String content);
+    void ingestpdf(MultipartFile file);
 }
