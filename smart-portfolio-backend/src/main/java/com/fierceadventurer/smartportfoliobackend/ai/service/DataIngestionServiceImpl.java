8480760f5f01e585ce6adccc3812914e6357af63
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-10 00:17:24 +0530

    feat: wrote a service logic to break pdf into chunks read it and save it in vectordb.

--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/service/DataIngestionServiceImpl.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/service/DataIngestionServiceImpl.java
@@ -2,10 +2,12 @@ package com.fierceadventurer.smartportfoliobackend.ai.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 import org.springframework.ai.document.Document;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
@@ -17,17 +19,25 @@ public class DataIngestionServiceImpl implements DataIngestionService{
     private final VectorStore vectorStore;
 
     @Override
-    public void ingestData(String content) {
-        log.info("Starting data ingestion process...");
-        Document rawDocument = new Document(content, Map.of("source", "portfolio-resume"));
+    public void ingestPdf(MultipartFile file) {
+        log.info("Starting pdf ingestion process for file: {}", file.getOriginalFilename());
+        try{
+            PagePdfDocumentReader pdfReader = new PagePdfDocumentReader(file.getResource());
 
-        TokenTextSplitter splitter = new TokenTextSplitter();
-        List<Document> chunkedDocuments = splitter.apply(List.of(rawDocument));
+            List<Document> extractedPages = pdfReader.get();
+            log.info("Successfully extracted {} pages from the PDF.", extractedPages.size());
 
-        log.info("Split resume into {} chunks. Generating embeddings and saving to pgvector...", chunkedDocuments.size());
+            TokenTextSplitter splitter = new TokenTextSplitter();
+            List<Document> chunkedDocuments = splitter.apply(extractedPages);
 
-        vectorStore.add(chunkedDocuments);
+            log.info("Split PDF into {} chunks. Generating embeddings and saving to pgvector...", chunkedDocuments.size());
+            vectorStore.add(chunkedDocuments);
 
-        log.info("Successfully ingested all data into the vector store!");
+            log.info("Successfully ingested PDF into the vector store!");
+
+        }catch (Exception e) {
+            log.error("Failed to parse and ingest PDF document", e);
+            throw new RuntimeException("Failed to process PDF file. Ensure it is a valid text-based PDF.", e);
+        }
     }
 }
