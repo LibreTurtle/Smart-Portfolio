b871a25eb2d02b273543e95da3525f21da99e968
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-09 23:26:26 +0530

    fix: changed HybridRagService.java for sse using webflux

--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/dto/ChatRequest.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/dto/ChatRequest.java
@@ -5,5 +5,4 @@ import jakarta.validation.constraints.NotBlank;
 public record ChatRequest(
         @NotBlank(message = "Question cannot be empty")
         String Question
-)  {
-}
+){}
--- smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/service/HybridRagService.java
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/ai/service/HybridRagService.java
@@ -2,8 +2,8 @@ package com.fierceadventurer.smartportfoliobackend.ai.service;
 
 import com.fierceadventurer.smartportfoliobackend.ai.dto.ChatRequest;
 import org.springframework.ai.chat.model.ChatResponse;
+import reactor.core.publisher.Flux;
 
 public interface HybridRagService {
-    ChatResponse askQuestion(ChatRequest request);
-
+    Flux<String> streamQuestion(ChatRequest request);
 }
