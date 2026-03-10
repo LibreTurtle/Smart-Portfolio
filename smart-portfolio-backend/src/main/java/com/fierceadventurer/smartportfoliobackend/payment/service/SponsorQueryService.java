61140417ac9042e9addf80db87f07423a26f1f49
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-11 02:24:36 +0530

    feat: created SponsorQueryService to fetch last 10 sponsors from db.

---
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/payment/service/SponsorQueryService.java
@@ -0,0 +1,27 @@
+package com.fierceadventurer.smartportfoliobackend.payment.service;
+
+import com.fierceadventurer.smartportfoliobackend.payment.dto.SponsorResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SponsorQueryService {
+    private final JdbcClient jdbcClient;
+
+    public List<SponsorResponse> getRecentSponsor(){
+        String sql = """
+            SELECT sponsor_name AS sponsorName, amount, currency 
+            FROM sponsors 
+            WHERE status = 'SUCCESS' 
+            ORDER BY created_at DESC 
+            LIMIT 10
+        """;
+
+        return jdbcClient.sql(sql).query(SponsorResponse.class)
+                .list();
+    }
+}
