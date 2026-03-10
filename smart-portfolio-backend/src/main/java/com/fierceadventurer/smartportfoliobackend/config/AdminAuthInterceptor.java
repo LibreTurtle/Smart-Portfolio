3b7652caa7e3de30e914d7c294e4b9f1ed57069e
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-11 02:10:47 +0530

    feat: create AdminAuthInterceptor to project admin endpoints through cors and secret key.

---
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/config/AdminAuthInterceptor.java
@@ -0,0 +1,36 @@
+package com.fierceadventurer.smartportfoliobackend.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+public class AdminAuthInterceptor implements HandlerInterceptor {
+    @Value("${portfolio.admin-secret}")
+    private String adminSecret;
+
+    @Override
+    public boolean prehandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        // cors preflight request is allowed
+        if(request.getMethod().equalsIgnoreCase("OPTIONS")){
+            return true;
+        }
+
+        String providedSecret = request.getHeader("X-Admin_Secret");
+
+        if(providedSecret != null && providedSecret.equals(adminSecret)){
+            return true;
+        }
+        else {
+            log.warn("Unauthorized access attempt to admin route from IP: {}", request.getRemoteAddr());
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.getWriter().write("401 Unauthorized: Invalid or missing Admin Secret");
+            return false; // Block the request
+        }
+    }
+}
