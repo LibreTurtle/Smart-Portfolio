48adc0e7108cbdd705873c6b6c9ecb222284f757
fierce-adventurer <tripathiayush589@gmail.com>
2026-03-11 00:49:00 +0530

    feat: added ratelimit interceptor to CorsConfig

---
+++ smart-portfolio-backend/src/main/java/com/fierceadventurer/smartportfoliobackend/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.fierceadventurer.smartportfoliobackend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig implements WebMvcConfigurer{
+    @Value("${portfolio.frontend-url}")
+    private String frontendUrl;
+
+    private final RateLimitInterceptor rateLimitInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(frontendUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/api/chat");
+    }
+}
